895bd71963c7b79bb0b8d05ce5deb5c3a7b72c82
Ujjwalsri03 <ujjawalshriwastav9771@gmail.com>
2024-10-01 00:24:05 +0530

    enhance localstorage

--- StudyPlanner/src/App.jsx
+++ StudyPlanner/src/App.jsx
@@ -2,61 +2,56 @@ import React, { useState, useEffect } from 'react';
 import Subject from './Components/Subjects/AllSubject';
 import AddSubjectForm from './Components/AddSubject/AddSubject';
 
-
 function App() {
-  
   const [subjects, setSubjects] = useState([]);
 
-  
-
   useEffect(() => {
-    localStorage.setItem('subjects', JSON.stringify(subjects));
-  }, [subjects]);
-
-  useEffect(() => {
-    const storedSubjects = JSON.parse(localStorage.getItem('subjects'));
+    const storedSubjects = localStorage.getItem('subjects');
     if (storedSubjects) {
-      setSubjects(storedSubjects);
+      setSubjects(JSON.parse(storedSubjects));
     }
   }, []);
 
-  const addSubject = (name , hours) => {
-    setSubjects([...subjects, { name, hours }]);
+  
+  const addSubject = (name, hours) => {
+    const updatedSubjects = [...subjects, { name, hours }];
+    setSubjects(updatedSubjects);
+    localStorage.setItem('subjects', JSON.stringify(updatedSubjects)); // Store in localStorage
   };
 
   // Function to update hours for a subject
   const updateHours = (index, plusMinus) => {
-    
     const newSubjects = [...subjects];
-
-    const subjectToUpdate = newSubjects[index];
-  
-    subjectToUpdate.hours += plusMinus;
-
+    newSubjects[index].hours += plusMinus;
     setSubjects(newSubjects);
+    localStorage.setItem('subjects', JSON.stringify(newSubjects)); // Update in localStorage
   };
 
   return (
-    <div className="App" 
-     style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      padding: '3rem',
-      boxShadow: '0px 0px 7px black',
-      width: '50%',
-      margin: 'auto',
-      marginTop: '6rem',
-      borderRadius:'5px'
-    }}>
-      <h1 
+    <div
+      className="App"
       style={{
-        paddingBottom: '1.4rem',
-        fontFamily:'monospace',
-        fontSize: '35px',
-        fontStyle: 'oblique'
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '3rem',
+        boxShadow: '0px 0px 7px black',
+        width: '50%',
+        margin: 'auto',
+        marginTop: '6rem',
+        borderRadius: '5px',
       }}
-      > Self Study Planner</h1>
+    >
+      <h1
+        style={{
+          paddingBottom: '1.4rem',
+          fontFamily: 'monospace',
+          fontSize: '35px',
+          fontStyle: 'oblique',
+        }}
+      >
+        Self Study Planner
+      </h1>
       <AddSubjectForm addSubject={addSubject} />
       {subjects.map((subject, index) => (
         <Subject
